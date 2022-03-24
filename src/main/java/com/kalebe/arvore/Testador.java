3bab55afe484c1d3e60d912dc94418d9972ded04
kalebe <kalebeadv66@gmail.com>
2022-03-24 10:42:08 -0500

    adicionando novos métodos e retirando/refatorando métodos inúteis

--- src/main/java/com/kalebe/arvore/Testador.java
+++ src/main/java/com/kalebe/arvore/Testador.java
@@ -15,58 +15,103 @@ import java.util.List;
 public class Testador {
 
     private List<String> vasos = new ArrayList<>();
-    private Regra regra;
-    
+
     
     public void guarda(Vaso vaso_menor, Vaso vaso_maior) 
     {
-        this.vasos.add("(" + vaso_menor.getVolume().toString()+ "," 
-                    + vaso_maior.getVolume().toString() + ")");
+        this.vasos.add(("(" + vaso_menor.getVolume().toString()+ "," 
+                    + vaso_maior.getVolume().toString() + ")"));
     }
 
-    private Boolean valida_vaso(Vaso vaso_menor, Vaso vaso_maior) 
+    public Boolean valida_vaso(Vaso vaso_menor, Vaso vaso_maior) 
     {
         for (int i = 0; i < this.vasos.size(); i++) {
             if (("("+ vaso_menor.getVolume().toString() + "," + 
-                      vaso_maior.getVolume().toString() + ")") == this.vasos.get(i)) {
+                      vaso_maior.getVolume().toString() + ")").equals(this.vasos.get(i))) {
                 
                 return true;
             }
         }
         return false;
     }
-    
-    public void construir_(Vaso vaso_menor, Vaso vaso_maior)
+
+    public void encheVaso(Vaso vaso)
+   {
+      vaso.setVolume(vaso.getCapacidade());
+   }
+   
+   public void esvaziaVaso(Vaso vaso)
+   {
+       vaso.setVolume(0);
+   }
+   
+   public void despejarNoMaior(Vaso vaso_menor, Vaso vaso_maior)
+   {
+       if((vaso_menor.getVolume() + vaso_maior.getVolume()) > vaso_maior.getCapacidade())
+       {
+           vaso_menor.setVolume((vaso_menor.getVolume() + vaso_maior.getVolume())
+                                 - vaso_maior.getCapacidade());
+           vaso_maior.setVolume(vaso_maior.getCapacidade());
+       } 
+       else if((vaso_menor.getVolume() + vaso_maior.getVolume()) <= vaso_maior.getCapacidade())
+       {
+           vaso_maior.setCapacidade(vaso_menor.getVolume() + vaso_maior.getVolume());
+           vaso_menor.setVolume(0);
+       }
+       
+   }
+   
+   public void despejarNoMenor(Vaso vaso_menor, Vaso vaso_maior)
+   {
+       if((vaso_menor.getVolume()+ vaso_maior.getVolume()) > vaso_menor.getCapacidade())
+       {
+           
+           vaso_maior.setVolume((vaso_menor.getVolume() + vaso_maior.getVolume())
+                                 - vaso_menor.getCapacidade());
+           vaso_menor.setVolume(vaso_menor.getCapacidade());
+       }
+       else if((vaso_menor.getVolume() + vaso_maior.getVolume()) 
+               <= vaso_menor.getCapacidade())
+       {
+           vaso_menor.setVolume(vaso_maior.getVolume() + vaso_menor.getVolume());
+           vaso_maior.setVolume(0);
+       }
+    }
+   
+    public void contruir_(Vaso vaso_menor, Vaso vaso_maior)
     {
-        if(vasos.get(0) == null) {vasos.add("(0,0)");}
-        
         if(vaso_menor.getVolume() == 0 && vaso_maior.getVolume() == 0)
         {
-            if(valida_vaso(vaso_menor, vaso_maior) == false )
-            {
-                guarda(vaso_menor, vaso_maior);
-            }
+            encheVaso(vaso_menor);
+            guarda(vaso_menor, vaso_maior);
+            encheVaso(vaso_maior);
+            guarda(vaso_menor, vaso_maior);
         }
-        if(vaso_menor.getVolume() == 0)
+        if(vaso_menor.getVolume() > vaso_maior.getVolume())
         {
-           regra.esvaziaVaso(vaso_menor);
-            System.out.println(vaso_menor.getVolume());
+            despejarNoMaior(vaso_menor, vaso_maior);
+            guarda(vaso_menor, vaso_maior);
+        }
+        if(vaso_maior.getVolume() == vaso_maior.getCapacidade())
+        {
+            despejarNoMenor(vaso_menor, vaso_maior);
+            guarda(vaso_menor, vaso_maior);
+        }
+        if(vaso_maior.getVolume() < vaso_maior.getCapacidade())
+        {
+            
+                encheVaso(vaso_maior);
+                despejarNoMenor(vaso_menor, vaso_maior);
+                guarda(vaso_menor, vaso_maior);
+            
         }
         
-        
     }
-
-    public Regra getRegra() {
-        return regra;
-    }
-
-    public void setRegra(Regra regra) {
-        this.regra = regra;
-    }
-
+   
     public List<String> getVasos() {
         return vasos;
     }
      
+    
     
 }
